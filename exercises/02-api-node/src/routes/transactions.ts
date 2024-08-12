11c1baf80d96e46efe45eb91db6a0ed8da711e77
= <jesse.bcorreia@gmail.com>
2024-08-12 18:16:21 -0300

    /exercises/02-api-node | middlewares e cookies no fastify
    Introdução aos conceitos de middlewares e cookies, aplicados ao fastify. Também foram feitos ajustes nas rotas

--- exercises/02-api-node/src/routes/transactions.ts
+++ exercises/02-api-node/src/routes/transactions.ts
@@ -3,24 +3,40 @@ import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
 
 import { knex } from '../database'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function transactionRoutes(app: FastifyInstance) {
   // como o método app.register do fastify no server.js já está definindo o prefix "/transactions", todas as rotas iniciarão assim por padrão
 
-  app.get('/summary', async () => {
-    const summary = await knex('transactions')
-      .sum('amount', { as: 'amount' })
-      .first()
-    return { summary }
+  app.addHook('preHandler', async (request) => {
+    // exemplo de middleware global aplicável apenas às rotas /transactions
+    console.log(`[${request.method}] ${request.url}`)
   })
 
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select()
+  app.get(
+    '/summary',
+    { preHandler: [checkSessionIdExists] },
+    async (request) => {
+      const { sessionId } = request.cookies
+      const summary = await knex('transactions')
+        .where('session_id', sessionId)
+        .sum('amount', { as: 'amount' })
+        .first()
+      return { summary }
+    },
+  )
+
+  app.get('/', { preHandler: [checkSessionIdExists] }, async (request) => {
+    const { sessionId } = request.cookies
+    const transactions = await knex('transactions')
+      .select()
+      .where('session_id', sessionId)
     // desta forma é possível adicionar mais informações na resposta
     return { transactions }
   })
 
-  app.get('/:id', async (request) => {
+  app.get('/:id', { preHandler: [checkSessionIdExists] }, async (request) => {
+    const { sessionId } = request.cookies
     // validação do request.body com a biblioteca ZOD
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
@@ -28,7 +44,10 @@ export async function transactionRoutes(app: FastifyInstance) {
     const { id } = getTransactionParamsSchema.parse(request.params)
     const transaction = await knex('transactions')
       .select()
-      .where('id', id)
+      .where({
+        session_id: sessionId,
+        id,
+      })
       .first()
 
     return { transaction }
@@ -41,14 +60,26 @@ export async function transactionRoutes(app: FastifyInstance) {
       amount: z.number(),
       type: z.enum(['credit', 'debit']),
     })
+
     const { title, amount, type } = createTransactionBodySchema.parse(
       request.body,
     )
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 60s * 60m * 24h * 7d = 7 days
+      })
+    }
 
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return reply.status(201).send()
--- exercises/02-api-node/src/server.ts
+++ exercises/02-api-node/src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 import { env } from './env'
 import { transactionRoutes } from './routes/transactions'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
+app.register(cookie) // plugin para cookies
 app.register(transactionRoutes, {
   // com o prefix, todas as rotas aqui dentro iniciarão com /transactions
   prefix: 'transactions',
