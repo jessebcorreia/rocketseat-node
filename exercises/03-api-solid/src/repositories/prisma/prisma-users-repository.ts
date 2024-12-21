48b8d7a1750de913293fe91fbc74991aca00a325
Jessé Bruno Correia <123418831+jessebcorreia@users.noreply.github.com>
2024-12-21 11:20:01 +0000

    /exercises/03-api-solid | implementação dos repositórios do prisma

--- exercises/03-api-solid/src/repositories/prisma/prisma-users-repository.ts
+++ exercises/03-api-solid/src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,16 @@
 import { prisma } from '@/lib/prisma'
-import { Prisma } from 'prisma/prisma-client' // importa o objeto que contém a tipagem (UsersCreateInput)
+import { Prisma, Users } from 'prisma/prisma-client' // importa o objeto que contém a tipagem (UsersCreateInput)
 import { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string): Promise<Users | null> {
+    // o método findUnique() só busca chaves primárias ou registros únicos @unique
+    const user = await prisma.users.findUnique({
+      where: { id },
+    })
+    return user
+  }
+
   async findByEmail(email: string) {
     // o método findUnique() só busca chaves primárias ou registros únicos @unique
     const user = await prisma.users.findUnique({
