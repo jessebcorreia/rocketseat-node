d2342630b9a6b74d3b99c66f5215b4f5076259dc
Jessé Bruno Correia <jesse.bcorreia@gmail.com>
2025-04-18 23:25:58 -0300

    /exercises/04-clean-ddd | criação dos getters e setters

--- exercises/04-clean-ddd/src/domain/entities/question.ts
+++ exercises/04-clean-ddd/src/domain/entities/question.ts
@@ -1,6 +1,8 @@
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
+import dayjs from "dayjs"
 
 interface QuestionsProps {
   authorId: UniqueEntityID
@@ -14,4 +16,76 @@ interface QuestionsProps {
 
 export class Question extends Entity<QuestionsProps> {
 
+  get isNew(): boolean {
+    const daysBetweenTodayAndCratedAt = dayjs().diff(this.createdAt, 'days')
+    return daysBetweenTodayAndCratedAt < 3
+  }
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = bestAnswerId
+    this.touch()
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  set title(title: string) {
+    this.props.title = title
+    this.props.slug = Slug.createFromText(title)
+    this.touch()
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  get excerpt() {
+    return this.content
+      .substring(0, 120)
+      .trimEnd()
+      .concat('...')
+  }
+
+  get slug() {
+    return this.props.slug
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  static create(
+    props: Optional<QuestionsProps, 'createdAt' | 'slug' >,
+    id?: UniqueEntityID,
+  ) {
+    const question = new Question({
+      ...props,
+      slug: props.slug ?? Slug.createFromText(props.title),
+      createdAt: new Date(),
+    }, id)
+
+    return question
+  }
 }
