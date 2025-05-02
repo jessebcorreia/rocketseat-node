b6bc42cb8b964f28553eebc7354c78fd528bae7e
Jessé Bruno Correia <jesse.bcorreia@gmail.com>
2025-05-01 22:49:18 -0300

    /exercises/05-nest-clean | correção das importações dentro dos controllers

--- exercises/05-nest-clean/src/infra/http/controllers/fetch-recent-questions.controller.e2e-spec.ts
+++ exercises/05-nest-clean/src/infra/http/controllers/fetch-recent-questions.controller.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppModule } from '@/infra/app.module'
-import { PrismaService } from '@/infra/prisma/prisma.service'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { NestApplication } from '@nestjs/core'
 import { JwtService } from '@nestjs/jwt'
 import { Test } from '@nestjs/testing'
