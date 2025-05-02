ae8d7debfeb21d21a5a5cf501bcc63c01f6a591f
Jessé Bruno Correia <jesse.bcorreia@gmail.com>
2025-05-01 23:41:43 -0300

    /exercises/05-nest-clean | correção da importação do DatabaseModule dentro do arquivo `http.module.ts`

--- exercises/05-nest-clean/src/infra/http/http.module.ts
+++ exercises/05-nest-clean/src/infra/http/http.module.ts
@@ -3,7 +3,7 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
-import { DatabaseModule } from '@faker-js/faker/.'
+import { DatabaseModule } from '../database/database.module'
 
 @Module({
   imports: [DatabaseModule],
