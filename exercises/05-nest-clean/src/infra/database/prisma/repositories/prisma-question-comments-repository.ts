a243350179c651c3e59a5b496baf5573bd565c59
Jessé Bruno Correia <jesse.bcorreia@gmail.com>
2025-05-05 23:29:34 -0300

    /exercises/05-nest-clean | criação do controller para deletar comentário da pergunta

--- exercises/05-nest-clean/src/infra/database/prisma/repositories/prisma-question-comments-repository.ts
+++ exercises/05-nest-clean/src/infra/database/prisma/repositories/prisma-question-comments-repository.ts
@@ -52,7 +52,7 @@ export class PrismaQuestionCommentsRepository
   }
 
   async delete(questionComment: QuestionComment): Promise<void> {
-    await this.prisma.question.delete({
+    await this.prisma.comment.delete({
       where: {
         id: questionComment.id.toString(),
       },
