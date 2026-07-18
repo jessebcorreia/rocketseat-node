13151e29fa5ff693a78fcf40b4548e4a9cfae63d
Jessé Bruno Correia <jesse.bcorreia@gmail.com>
2026-07-18 02:13:52 -0300

    /exercises/05-nest-clean | configuração do eslint/prettier

---
+++ exercises/05-nest-clean/eslint.config.mjs
@@ -0,0 +1,26 @@
+import eslint from '@eslint/js';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  prettierConfig,
+);
