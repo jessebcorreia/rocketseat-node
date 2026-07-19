0e913cd00440fbdd25df0e53d9767437ebf0ab6f
Jessé Bruno Correia <jesse.bcorreia@gmail.com>
2026-07-19 13:24:34 -0300

    /exercises/05-nest-clean | implementa fluxo para retornar comentários de respostas com dados do autor

--- exercises/05-nest-clean/src/infra/database/prisma/repositories/prisma-answer-comments-repository.ts
+++ exercises/05-nest-clean/src/infra/database/prisma/repositories/prisma-answer-comments-repository.ts
@@ -4,6 +4,8 @@ import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { PrismaAnswerCommentMapper } from '../mappers/prisma-answer-comment-mapper';
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author';
+import { PrismaCommentWithAuthorMapper } from '../mappers/prisma-comment-with-author-mapper';
 
 @Injectable()
 export class PrismaAnswerCommentsRepository implements AnswerCommentsRepository {
@@ -36,6 +38,27 @@ export class PrismaAnswerCommentsRepository implements AnswerCommentsRepository 
     });
 
     return answerComment.map(PrismaAnswerCommentMapper.toDomain);
+  }
+
+  async findManyByAnswerIdWithAuthor(
+    answerId: string,
+    { page }: PaginationParams,
+  ): Promise<CommentWithAuthor[]> {
+    const comments = await this.prisma.comment.findMany({
+      where: {
+        answerId,
+      },
+      include: {
+        author: true,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    });
+
+    return comments.map(PrismaCommentWithAuthorMapper.toDomain);
   }
 
   async create(answerComment: AnswerComment): Promise<void> {
